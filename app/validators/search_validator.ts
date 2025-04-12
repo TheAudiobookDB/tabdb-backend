a450c0b10e89066412670bcb7f5e4868c86fae0d
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2025-04-12 15:36:33 +0200

    validation and logging

--- app/validators/search_validator.ts
+++ app/validators/search_validator.ts
@@ -1,5 +1,5 @@
 import vine from '@vinejs/vine'
-import { languageValidation } from '#config/app'
+import { languageValidation, pageValidation } from '#config/app'
 
 export const searchBookValidation = vine.object({
   title: vine.string().trim().minLength(3).maxLength(1023).optional(),
@@ -18,7 +18,7 @@ export const searchBookValidation = vine.object({
   isExplicit: vine.boolean().optional(),
   isAbridged: vine.boolean().optional(),
   type: vine.enum(['book', 'audiobook', 'podcast', 'e-book']).optional(),
-  page: vine.number().withoutDecimals().min(1).optional(),
+  page: pageValidation,
   threshold: vine.number().min(0.25).max(1).optional(),
 })
 
@@ -27,7 +27,7 @@ export const searchBookValidator = vine.compile(searchBookValidation)
 const createSearchValidator = vine.object({
   name: vine.string().trim().minLength(3).maxLength(1023).optional().requiredIfMissing('keywords'),
   keywords: vine.string().trim().minLength(3).maxLength(1023).optional().requiredIfMissing('name'),
-  page: vine.number().withoutDecimals().min(1).optional(),
+  page: pageValidation,
   threshold: vine
     .number()
     .min(0.25)
@@ -44,7 +44,7 @@ export const searchGenreValidator = vine.compile(
   vine.object({
     name: vine.string().trim().minLength(3).maxLength(1023),
     type: vine.enum(['genre', 'tag']).optional(),
-    page: vine.number().withoutDecimals().min(1).optional(),
+    page: pageValidation,
     threshold: vine.number().min(0.25).max(1).optional(),
   })
 )
